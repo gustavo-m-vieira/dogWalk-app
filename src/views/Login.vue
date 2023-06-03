1dd0f42a3d524e072ccb790205e9d1cc574166d1
Lucas Ikuhara <lri2911@gmail.com>
2023-06-02 21:07:46 -0300

    feat: start signup component

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sign-up">
+    f
+  </div>
+</template>
+
+<style>
+.sign-up {
+  display: flex;
+  background: red;
+}
+.modal {
+  background-color: aqua;
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="fullscreen">
+    <div class="modal">
+      <p>First Name:</p>
+      <p>Last Name:</p>
+    </div>
+  </div>
+</template>
+
+<style>
+
+.modal {
+  background-color: var(--modal-bg-color-light);
+  width: 80%;
+  padding: 5%;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
