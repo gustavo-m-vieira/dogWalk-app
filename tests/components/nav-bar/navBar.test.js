f7a3fd5cc562f11463425a8295497d5d1bf51205
Lucas Ikuhara <lri2911@gmail.com>
2023-06-13 23:20:29 -0300

    feat: add nav bar visibility tests

---
+++ tests/components/nav-bar/navBar.test.js
@@ -0,0 +1,30 @@
+import navBar from "../../../src/components/nav-bar/navBar.js"
+
+
+describe("NavBar", () => {
+
+    beforeEach(() => {
+        localStorage.clear()
+    })
+
+    it("should be visible if the 'loggedIn' localStorage value is set to true", () => {
+
+        localStorage.setItem("loggedIn", true)
+        const result = navBar.methods.visible()
+    
+        expect(result).toBeDefined()
+        expect(result).toBeTruthy()
+    })
+
+    it("should not be visible if the 'loggedIn' localStorage value is set to anything other than true or undefined", () => {
+
+        expect(navBar.methods.visible()).toBeFalsy()
+
+        localStorage.setItem("loggedIn", false)
+        expect(navBar.methods.visible()).toBeFalsy()
+
+        localStorage.setItem("loggedIn", "other")
+        expect(navBar.methods.visible()).toBeFalsy()
+    })
+  
+})
